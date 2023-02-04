36a6b3ae2efc205d9fc424c06628f385cd51ade5
Shivakc7 <shivacr77@gmail.com>
2023-02-04 09:49:48 +0545

    did some timing changes

---
+++ src/components/Timer.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+const Timer = ({ setStopTime, questionNumber }) => {
+  const [timer, setTimer] = useState(30);
+
+  useEffect(() => {
+    if (timer === 0) return setStopTime(true);
+    const interval = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [setStopTime, timer]);
+
+  useEffect(() => {
+    setTimer(30);
+  }, [questionNumber]);
+
+  return timer;
+};
+
+export default Timer;
