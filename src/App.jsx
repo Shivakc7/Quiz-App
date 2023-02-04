8ed106f8347af1e436df5ea079238e20c97fa498
Shivakc7 <shivacr77@gmail.com>
2023-02-04 09:51:21 +0545

    helllllll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
+import { Start } from "./components/Start";
+import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
 import { moneylist, questions } from "./data";
 
 function App() {
+  const [username, setUsername] = useState(null);
   const [questionNumber, setQuestionNumber] = useState(1);
   const [stopTime, setStopTime] = useState(false);
   const [earn, setEarn] = useState("$ 0");
@@ -20,42 +23,53 @@ function App() {
 
   return (
     <div className="app">
-      <div className="main">
-        {stopTime ? (
-          <h1 className="earnText">You earned : {earn} </h1>
-        ) : (
-          <>
-            <div className="top">
-              <div className="timer">30</div>
-            </div>
-            <div className="bottom">
-              <Trivia
-                questions={questions}
-                setStopTime={setStopTime}
-                setQuestionNumber={setQuestionNumber}
-                questionNumber={questionNumber}
-              />
-            </div>
-          </>
-        )}
-      </div>
-      <div className="pyramid">
-        <ul className="moneyList">
-          {moneylist.map((money) => (
-            <li
-              key={money.id}
-              className={
-                questionNumber === money.id
-                  ? "moneyListItem active"
-                  : "moneyListItem"
-              }
-            >
-              <span className="moneyListNumber">{money.id}</span>
-              <span className="moneyListAmount">{money.amount}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {username ? (
+        <>
+          <div className="main">
+            {stopTime ? (
+              <h1 className="earnText">You earned : {earn} </h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setStopTime={setStopTime}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    questions={questions}
+                    setStopTime={setStopTime}
+                    setQuestionNumber={setQuestionNumber}
+                    questionNumber={questionNumber}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneylist.map((money) => (
+                <li
+                  key={money.id}
+                  className={
+                    questionNumber === money.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListNumber">{money.id}</span>
+                  <span className="moneyListAmount">{money.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <Start setUsername={setUsername} />
+      )}
     </div>
   );
 }
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import useSound from "use-sound";
+import play from "../assets/sounds/play.mp3";
+import correct from "../assets/sounds/correct.mp3";
+import wrong from "../assets/sounds/wrong.mp3";
 
 const Trivia = ({
   questions,
@@ -6,10 +10,16 @@ const Trivia = ({
   setQuestionNumber,
   questionNumber,
 }) => {
-  console.log(questions);
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
 
   useEffect(() => {
     setQuestion(questions[questionNumber - 1]);
@@ -27,16 +37,20 @@ const Trivia = ({
     delay(3000, () =>
       setClassName(a.correct ? "answer correct" : "answer wrong")
     );
-    delay(6000, () =>
-      {
-        if(a.correct){
-          setQuestionNumber(prev => prev + 1)
-          setSelectedAnswer(null)
-        }else{
-          setStopTime(true)
-        }
+    delay(5000, () => {
+      if (a.correct) {
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
+      } else {
+        wrongAnswer()
+        delay(1000, () => {
+          setStopTime(true);
+        })
       }
-    );
+    });
   };
 
   return (
